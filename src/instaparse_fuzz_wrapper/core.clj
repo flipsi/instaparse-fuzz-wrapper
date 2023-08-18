d0e7eb10759aee578bcd1397921acd50d2261642
Philipp Moers <soziflip@gmail.com>
2023-08-18 13:09:48 +0200

    src: Fuzz the parsing, not the parser

--- src/instaparse_fuzz_wrapper/core.clj
+++ src/instaparse_fuzz_wrapper/core.clj
@@ -2,6 +2,14 @@
   (:require [com.code-intelligence.jazzer-clj.core :as fuzzing]
             [instaparse.core :as insta]))
 
+(def phone-uri-grammar-data (slurp "src/instaparse_fuzz_wrapper/phone_uri.txt"))
+
+(def phone-uri-grammar (insta/parser phone-uri-grammar-data))
+
 (fuzzing/deftarget instaparse_fuzz_wrapper.targets.first_try [input]
                    (let [string (.consumeRemainingAsString input)]
-                     (insta/parser string)))
+                     (try (phone-uri-grammar string)
+                          (catch Throwable e
+                            (when-not (re-matches #"Error parsing grammar specification" ;; FIXME
+                                                  (ex-message e))
+                              (throw e))))))
